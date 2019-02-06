96d361cfe82be089524294ee4a5ac793c7186a81
Evgeny_Joker <yas-shaman@yandex.ru>
2019-02-06 21:04:42 +0300

    v06.02.2019_less1
    Допилил урок 1
    приклеил футер книзу
    добавил на все страницы стрелочку наверх страницы

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//========== Вкладка действий с БД ==========//
+//========== УРОК 1 | вкладки ==========//
 	$(document).ready(function(){
 		$("#less1_cmd_button").on('click', function(){
 		if ($("#less1_cmd").is(":hidden")) {
@@ -6,6 +6,10 @@
 			
 			$("#less1_myadmin").hide("slow");
 			$("#less1_php").hide("slow");
+			
+			document.getElementById('less1_cmd_button').className = 'less less1 less_activ';
+			document.getElementById('less1_myadmin_button').className = 'less less1';
+			document.getElementById('less1_php_button').className = 'less less1';
         } else {
             $("#less1_cmd").hide("slow");
         }
@@ -17,6 +21,10 @@
 			
 			$("#less1_cmd").hide("slow");
 			$("#less1_php").hide("slow");
+			
+			document.getElementById('less1_cmd_button').className = 'less less1';
+			document.getElementById('less1_myadmin_button').className = 'less less1 less_activ';
+			document.getElementById('less1_php_button').className = 'less less1';
         } else {
             $("#less1_myadmin").hide("slow");
         }
@@ -28,8 +36,24 @@
 			
 			$("#less1_myadmin").hide("slow");
 			 $("#less1_cmd").hide("slow");
+			 
+			document.getElementById('less1_cmd_button').className = 'less less1';
+			document.getElementById('less1_myadmin_button').className = 'less less1';
+			document.getElementById('less1_php_button').className = 'less less1 less_activ';
         } else {
             $("#less1_php").hide("slow");
         }
+		});
+	});
+	
+//========== УРОК 1 | PHP ==========//
+	$(document).ready(function(){
+		$("#less1_button_phpcode").on('click', function(){
+			var hostname = $("#less1_hostname").val();
+			var database = $("#less1_database").val();
+			var username = $("#less1_username").val();
+			var password = $("#less1_password").val();
+			$("#less1_polezapros").html("< ?php //без пробела <br> 	$db_hostname = '" + hostname + "'; <br> $db_database = '" + database + "'; <br> $db_username = '" + username + "'; <br> $db_password = '" + password + "'; <br> ?> </print>");
+			
 		});
 	});
--- script_heder_footer.js
+++ script_heder_footer.js
@@ -7,4 +7,17 @@ $(document).ready(function(){
 	$(".hed").html('<div class="men1"><a href="index.html"><img class="logo" src="logo.png"></a>	</div><div class="men"><a href="index.html">Что это?</a></div><div class="men"><a href="kostruct.html">Занятия</a></div><div class="men"><a href="faq.html">Справочник</a></div>');  
 	//выводит хедер во внутренних папках
 	$(".inner_hed").html('<div class="men1"><a href="../index.html"><img class="logo" src="logo.png"></a>	</div><div class="men"><a href="../index.html">Что это?</a></div><div class="men"><a href="../kostruct.html">Занятия</a></div><div class="men"><a href="../faq.html">Справочник</a></div>');
+
+	$(function() {
+		$(window).scroll(function() { 
+		if($(this).scrollTop() != 0) { 
+		$('#toTop').fadeIn();
+		} else {
+		$('#toTop').fadeOut();
+		}
+		});
+		$('#toTop').click(function() {
+		$('body,html').animate({scrollTop:0},800);
+		});
+	});
 });
